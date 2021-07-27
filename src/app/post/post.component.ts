1aed28c63d8460e3ee23142c65a30b536561f56e
Ravalison Manda <manda.ravalison@gmail.com>
2021-07-27 17:19:06 +0300

    Ajout de la fonctionnalité Love

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,12 +7,18 @@ import { Post } from '../model/Post';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  @Input() post:Post ;
+  @Input() post: Post ;
+  isLoved: boolean ;
   constructor() { 
     this.post = new Post() ;
+    this.isLoved = false ;
   }
 
   ngOnInit(): void {
   }
 
+  toggleLove($event:Event){
+    this.isLoved = !this.isLoved ;
+    $event.preventDefault() ;
+  }
 }
