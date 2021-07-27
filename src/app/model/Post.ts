19a15c70ae473501247292f1e53d69f8214b4330
Ravalison Manda <manda.ravalison@gmail.com>
2021-07-27 18:54:33 +0300

    Création du squelette du détail des posts

--- src/app/model/Post.ts
+++ src/app/model/Post.ts
@@ -3,13 +3,15 @@ export class Post{
     imagePath: string ;
     authorName: string ;
     authorPdpPath: string ;
-    date: string
+    date: string ;
+    description: string ;
 
-    constructor(id = 0, imagePath = '', authorName = '', authorPdpPath = '', date = ''){
+    constructor(id = 0, imagePath = '', authorName = '', authorPdpPath = '', date = '', description = ''){
         this.id = id ;
         this.imagePath = imagePath ;
         this.authorName = authorName ;
         this.authorPdpPath = authorPdpPath ;
         this.date = date ;
+        this.description = description ;
     }
 }
