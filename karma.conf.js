3a4cce22eebb854e77e3f1f62e0575c09b38ea84
Manda Ravalison <manda.ravalison@gmail.com>
2021-07-30 08:30:37 +0300

    Changement du nom du projet

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-first-project'),
+      dir: require('path').join(__dirname, './coverage/angular-instaPic'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-project'`, () => {
+  it(`should have as title 'angular-instaPic'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-project');
+    expect(app.title).toEqual('angular-instaPic');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-instaPic app is running!');
   });
 });
